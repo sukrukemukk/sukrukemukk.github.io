0a261643dba74ad5c5e8b0aaacbaa59b2642a7c3
sukrukemukk <sukru@kadriyemacit.com>
2025-03-19 23:16:33 +0300

    Update web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2237903627"
+    serviceWorkerVersion: "983382217"
   }
 });
